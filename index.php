7b16b5aef1ece104072c8c789b55bb08b9bd172c
Vaidmine <73877238+Vaidmine@users.noreply.github.com>
2021-02-08 18:32:06 +0200

    pirmas

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+echo 'uzduotis 1';
+echo '<br>';
+$vardas = 'Vardas';
+$pavarde = 'Pavarde';
+$gimimometai = 1980;
+$dabartiniaimetai = 2020;
+
+echo "As esu $vardas.$pavarde. Man yra $dabartiniaimetai - $gimimometai metu";
+ 
+?> 
+
+<?php
+
+$first_number = 10;
+$second_number = 20;
+$third_number = 100;
+
+$sum_total = $third_number - $second_number + $first_number;
+
+print ($sum_total);
+
+?>
